bafe74c1b914a98f1f2e907f1d5108a31cf00ffe
Víctor Martín Molina <vmartin@profile.es>
2020-03-03 13:14:19 +0100

    kotlin

--- source-demo-periodic/src/main/java/com/example/sourcedemoperiodic/SourceDemoPeriodicApplication.java
+++ source-demo-periodic/src/main/java/com/example/sourcedemoperiodic/SourceDemoPeriodicApplication.java
@@ -17,10 +17,6 @@ import java.util.function.Function;
 
 @SpringBootApplication
 public class SourceDemoPeriodicApplication {
-	static Flux<Integer> generateFlux() {
-		return Flux.interval(Duration.ofSeconds(1)).map(Long::intValue);
-	}
-
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SourceDemoPeriodicApplication.class, args);
 	}
@@ -34,29 +30,4 @@ public class SourceDemoPeriodicApplication {
 	public Consumer<String> oddLogger() {
 		return v -> System.out.println("Odd logger: " + v);
 	}
-
-	public static void main2(String[] args) throws IOException {
-		Function<Flux<Integer>, Tuple2<Flux<String>, Flux<String>>> router = flux -> {
-			Flux<Integer> connectedFlux = flux.publish().autoConnect(2);
-			UnicastProcessor even = UnicastProcessor.create();
-			UnicastProcessor odd = UnicastProcessor.create();
-
-			Flux<Integer> evenFlux = connectedFlux.filter(number -> number % 2 == 0)
-					.doOnNext(number -> even.onNext("EVEN: " + number));
-			Flux<Integer> oddFlux = connectedFlux.filter(number -> number % 2 != 0)
-					.doOnNext(number -> even.onNext("ODD: " + number));
-
-			return Tuples.of(Flux.from(even).doOnSubscribe(x -> evenFlux.subscribe()),
-					Flux.from(odd).doOnSubscribe(x -> oddFlux.subscribe()));
-		};
-		Tuple2<Flux<String>, Flux<String>> streams = router.apply(generateFlux());
-		streams.getT1().subscribe(v -> {
-			System.out.println(v);
-		});
-		streams.getT2().subscribe(v -> {
-			System.out.println(v);
-		});
-		System.in.read();
-	}
-
 }
