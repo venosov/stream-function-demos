c4e4d234944ea257218f48addae9105d4f121a8b
Víctor Martín Molina <vmartin@profile.es>
2020-03-04 17:24:44 +0100

    better with message

--- demo-function-web/src/main/kotlin/com/example/demofunctionweb/DemoFunctionWebApplication.kt
+++ demo-function-web/src/main/kotlin/com/example/demofunctionweb/DemoFunctionWebApplication.kt
@@ -3,13 +3,14 @@ package com.example.demofunctionweb
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.messaging.Message
 import java.util.function.Function
 
 @SpringBootApplication
 class DemoFunctionWebApplication {
 	@Bean
-	fun reverseString() = Function<String, String> {
-		val result = StringBuilder(it).reverse().toString()
+	fun reverseString() = Function { it: Message<String> ->
+		val result = StringBuilder(it.payload).reverse().toString()
 		println(result)
 		result
 	}
